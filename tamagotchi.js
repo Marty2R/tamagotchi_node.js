2be5d766cab84b8a50f84d8344fc183068dc0e45
Marty Rabatel <marty.rabatel2@gmail.com>
2022-12-21 16:05:19 +0100

    Ajout de la fonctionnalité prénom et ajout de l'accueil du jeu

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,21 +1,45 @@
 const fs = require('fs');
 
-let cat = [
-    ["cat_health", 100],
-    ["cat_hungry", 100],
-    ["cat_bored", 100]
-]
+console.log("---- PRESS SPACE FOR START ----");
+console.log(`
+            =^.^=
+                                `); 
 
-function createAndWrite (file, content) { // Crée le fichier texte de sauvegarde des données du chat.
+// Crée le fichier texte de sauvegarde des données du chat :
+function createAndWrite (file, content) {
 
     fs.writeFile(file, content, function (err) {
 
         if (err) throw err;
-        console.log('New file saved !');
 
     });
 
-} 
+}
 
-createAndWrite("cat_emotion.json", [["cat_health", 100], ["cat_hungry", 100], ["cat_bored", 100]]);
-createAndWrite("cat_emotion.json", "cat_emotion.json");
+// Récupère le nom du chat dans un prompt dans la console :
+function catName (params) { 
+
+    const readline = require('readline').createInterface({
+
+        input: process.stdin,
+        output: process.stdout
+
+    });
+
+    readline.question("Enter cat name : ", name => {
+
+        // Enregistre les informations du chat dans le fichier json :
+        createAndWrite("cat_data.json", `[["cat_name", "${name}"], ["cat_health", 100], ["cat_hungry", 100], ["cat_bored", 100]]`);
+        readline.close();
+
+    });
+    
+}
+
+function startGame () {
+
+    console.log("Welcome to Tamagotchi ! ");
+
+    catName();
+    
+} startGame();
